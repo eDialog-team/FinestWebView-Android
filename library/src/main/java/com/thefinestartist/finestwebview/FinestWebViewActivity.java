5954d3f06f266c1e7cddf1e6c8c816f397108310
Pierre Champion <pierrescalade@gmail.com>
2018-07-23 14:54:56 +0200

    Update FinestWebViewActivity.java

--- library/src/main/java/com/thefinestartist/finestwebview/FinestWebViewActivity.java
+++ library/src/main/java/com/thefinestartist/finestwebview/FinestWebViewActivity.java
@@ -58,6 +58,7 @@ import com.thefinestartist.utils.etc.APILevel;
 import com.thefinestartist.utils.service.ClipboardManagerUtil;
 import com.thefinestartist.utils.ui.DisplayUtil;
 import com.thefinestartist.utils.ui.ViewUtil;
+import android.webkit.JavascriptInterface;
 
 //MailTo Imports
 
@@ -776,6 +777,9 @@ public class FinestWebViewActivity extends AppCompatActivity
       if (webViewOffscreenPreRaster != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
         settings.setOffscreenPreRaster(webViewOffscreenPreRaster);
       }
+      
+      webView.addJavascriptInterface(new JavaScriptInterfaceBlob(getContext()), "Android");
+      settings.setPluginState(PluginState.ON);
 
       //            // Other webview options
       //            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
@@ -1339,4 +1343,69 @@ public class FinestWebViewActivity extends AppCompatActivity
       BroadCastManager.onPageCommitVisible(FinestWebViewActivity.this, key, url);
     }
   }
+}
+
+class JavaScriptInterfaceBlob {
+    private Context context;
+    private NotificationManager nm;
+    public JavaScriptInterface(Context context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data) throws IOException {
+        convertBase64StringToPdfAndStoreIt(base64Data);
+    }
+    public static String getBase64StringFromBlobUrl(String blobUrl){
+       if(blobUrl.startsWith("blob")){
+           return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', 'YOUR BLOB URL', true);" +
+                    "xhr.setRequestHeader('Content-type','application/pdf');" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobPdf = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobPdf);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            Android.getBase64FromBlobData(base64data);" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf) throws IOException {
+        final int notificationId = 1;
+        String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
+        final File dwldsPath = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS) + "/YourFileName_" + currentDateTime + "_.pdf");
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst("^data:application/pdf;base64,", ""), 0);
+        FileOutputStream os;
+        os = new FileOutputStream(dwldsPath, false);
+        os.write(pdfAsBytes);
+        os.flush();
+
+        if(dwldsPath.exists()) {
+            NotificationCompat.Builder b = new NotificationCompat.Builder(context, "MY_DL");
+                    .setDefaults(NotificationCompat.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_file_download_24dp)
+                    .setContentTitle("MY TITLE")
+                    .setContentText("MY TEXT CONTENT");
+            nm = (NotificationManager) this.context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if(nm != null) {
+                nm.notify(notificationId, b.build());
+                Handler h = new Handler();
+                long delayInMilliseconds = 5000;
+                h.postDelayed(new Runnable() {
+                    public void run() {
+                        nm.cancel(notificationId);
+                    }
+                }, delayInMilliseconds);
+            }
+        }
+    }
 }
