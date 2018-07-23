0b6e8a33986ec721c1f6eb9d466c2a3aca5ac3df
Drakirus <pierrescalade@gmail.com>
2018-07-23 16:17:43 +0200

    convertBase64StringToPdfAndStoreIt add type

--- library/src/main/java/com/thefinestartist/finestwebview/JavaScriptInterfaceBlob.java
+++ library/src/main/java/com/thefinestartist/finestwebview/JavaScriptInterfaceBlob.java
@@ -1,12 +1,14 @@
 package com.thefinestartist.finestwebview;
 
 import android.app.NotificationManager;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Environment;
-import android.os.Handler;
-import android.support.v4.app.NotificationCompat;
 import android.util.Base64;
 import android.webkit.JavascriptInterface;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -22,37 +24,33 @@ public class JavaScriptInterfaceBlob {
     }
 
     @JavascriptInterface
-    public void getBase64FromBlobData(String base64Data, String fileName) throws IOException {
-        convertBase64StringToPdfAndStoreIt(base64Data, fileName);
+    public void getBase64FromBlobData(String base64Data, String fileName, String type) throws IOException {
+        convertBase64StringToPdfAndStoreIt(base64Data, fileName, type);
     }
    
-    private void convertBase64StringToPdfAndStoreIt(String base64PDf, String fileName) throws IOException {
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf, String fileName, String type) throws IOException {
         final int notificationId = 1;
         String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
         final File dwldsPath = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_DOWNLOADS) + "/" + fileName);
-        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst("^data:application/pdf;base64,", ""), 0);
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst("^data:" + type.trim() + ";base64,", ""), 0);
         FileOutputStream os;
         os = new FileOutputStream(dwldsPath, false);
         os.write(pdfAsBytes);
         os.flush();
 
         if(dwldsPath.exists()) {
-            NotificationCompat.Builder b = new NotificationCompat.Builder(context, "MY_DL")
-                    .setDefaults(NotificationCompat.DEFAULT_ALL)
-                    .setWhen(System.currentTimeMillis())
-                    .setContentTitle("Smart'POS")
-                    .setContentText("Téléchargement..");
-            nm = (NotificationManager) this.context.getSystemService(Context.NOTIFICATION_SERVICE);
-            if(nm != null) {
-                nm.notify(notificationId, b.build());
-                Handler h = new Handler();
-                long delayInMilliseconds = 5000;
-                h.postDelayed(new Runnable() {
-                    public void run() {
-                        nm.cancel(notificationId);
-                    }
-                }, delayInMilliseconds);
+            Toast.makeText(context, "Ficher Télécharger!", Toast.LENGTH_SHORT).show();
+
+            Intent target = new Intent(Intent.ACTION_VIEW);
+            target.setDataAndType(Uri.fromFile(dwldsPath), type.trim());
+            target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+
+            Intent intent = Intent.createChooser(target, "Open File");
+            try {
+                context.startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(context, "Le ficher " + type + "n'a pas peu être ouvert\n(Application de lecture non-disponible)", Toast.LENGTH_SHORT).show();
             }
         }
     }
